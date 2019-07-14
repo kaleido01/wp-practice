d4ffe91cbe128af6d7d12e28f2453f4273b7baaa
kaleido <kaleido@gmail.com>
2019-07-15 00:24:57 +0900

    footer

--- footer.php
+++ footer.php
@@ -1,41 +1,46 @@
+<!-- footer-menu -->
+<div id="footer-menu">
+  <div class="inner">
+    <div class="footer-logo"><a href="/">TF-30</a></div><!-- /footer-logo -->
+    <div class="footer-sub">サブタイトルが入りますサブタイトルが入ります</div><!-- /footer-sub -->
 
-	<!-- footer-menu -->
-	<div id="footer-menu">
-		<div class="inner">
-			<div class="footer-logo"><a href="/">TF-30</a></div><!-- /footer-logo -->
-			<div class="footer-sub">サブタイトルが入りますサブタイトルが入ります</div><!-- /footer-sub -->
+    <nav class="footer-nav">
 
-			<nav class="footer-nav">
-				<ul class="footer-list">
-					<li class="menu-item"><a href="#">メニュー1</a></li>
-					<li class="menu-item"><a href="#">メニュー2</a></li>
-					<li class="menu-item"><a href="#">メニュー3</a></li>
-					<li class="menu-item"><a href="#">メニュー4</a></li>
-					<li class="menu-item"><a href="#">メニュー5</a></li>
-				</ul>
-			</nav>
+      <?php
+        wp_nav_menu(
+        //.footer-listを置き換えて、PC用メニューを動的に表示する
+        array(
+        'container' => false,
+        'depth' => 1,
+        'theme_location' => 'footer', //フッターメニューをここに表示すると指定
+        'container' => 'false',
+        'menu_class' => 'footer-list',
+        )
+        );
+      ?>
+    </nav>
 
-		</div><!-- /inner -->
-	</div><!-- /footer-menu -->
+  </div><!-- /inner -->
+</div><!-- /footer-menu -->
 
 
 
-	<!-- footer -->
-	<footer id="footer">
-		<div class="inner">
-			<div class="copy">&copy; daily-trial WordPress theme All rights reserved.</div><!-- /copy -->
-			<div class="by">Presented by <a href="https://tokyofreelance.jp/" rel="noopener" target="_blank">東京フリーランス</a>
-			</div><!-- /by -->
+<!-- footer -->
+<footer id="footer">
+  <div class="inner">
+    <div class="copy">&copy; daily-trial WordPress theme All rights reserved.</div><!-- /copy -->
+    <div class="by">Presented by <a href="https://tokyofreelance.jp/" rel="noopener" target="_blank">東京フリーランス</a>
+    </div><!-- /by -->
 
-		</div><!-- /inner -->
-	</footer><!-- /footer -->
+  </div><!-- /inner -->
+</footer><!-- /footer -->
 
-	<div class="floating">
-		<a href="#"><i class="fas fa-chevron-up"></i></a>
-	</div>
+<div class="floating">
+  <a href="#"><i class="fas fa-chevron-up"></i></a>
+</div>
 
-  <?php wp_footer(); ?>
-  
+<?php wp_footer(); ?>
+
 </html>
 </body>
 
--- header.php
+++ header.php
@@ -3,69 +3,83 @@
 
 <head>
 
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="format-detection" content="telephone=no">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
 
-	<title>TF-30</title>
-	<meta name="description" content="">
+  <title>TF-30</title>
+  <meta name="description" content="">
 
-	<meta property="og:title" content="TF-30">
-	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://example.com/">
-	<meta property="og:image" content="https://example.com/img/ogp.png">
-	<meta property="og:site_name" content="TF-30">
-	<meta property="og:description" content="">
-	<meta name="twitter:card" content="summary_large_image">
+  <meta property="og:title" content="TF-30">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://example.com/">
+  <meta property="og:image" content="https://example.com/img/ogp.png">
+  <meta property="og:site_name" content="TF-30">
+  <meta property="og:description" content="">
+  <meta name="twitter:card" content="summary_large_image">
 
 
   <?php wp_head(); ?>
-	<link rel="icon" href="./img/icon-home.png">
+  <link rel="icon" href="./img/icon-home.png">
 
 </head>
 
 <body>
 
-	<!-- header -->
-	<header id="header">
-		<div class="inner">
+  <!-- header -->
+  <header id="header">
+    <div class="inner">
+      <?php if (is_home() || is_front_page() ) : //トップページではロゴをh1に、それ以外のページではdivに。 ?>
+      <h1 class="header-logo"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
+      <!-- /header-logo -->
+      <?php else : ?>
+      <div class="header-logo"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></div>
+      <!-- /header-logo -->
+      <?php endif; ?>
+      <div class="header-sub"><?php bloginfo('description'); //ブログのdescriptionを表示 ?></div><!-- /header-sub -->
 
-			<h1 class="header-logo"><a href="/">TF-30</a></h1><!-- /header-logo -->
-			<div class="header-sub">サブタイトルが入りますサブタイトルが入ります</div><!-- /header-sub -->
 
-			<!-- drawer -->
-			<div class="drawer">
-				<div class="drawer-icon">
-					<span class="drawer-open"><i class="fas fa-bars"></i></span><!-- /drawer-open -->
-					<span class="drawer-close"><i class="fas fa-times"></i></span><!-- drawer-close -->
-				</div><!-- /drawer-icon -->
+      <!-- drawer -->
+      <div class="drawer">
+        <div class="drawer-icon">
+          <span class="drawer-open"><i class="fas fa-bars"></i></span><!-- /drawer-open -->
+          <span class="drawer-close"><i class="fas fa-times"></i></span><!-- drawer-close -->
+        </div><!-- /drawer-icon -->
+        <!-- drawer-content -->
+        <div class="drawer-content">
+          <?php
+            //.drawer-navを置き換えて、スマホ用メニューを動的に表示する
+            wp_nav_menu(
+            array(
+            'container' => false,
+            'depth' => 1,
+            'theme_location' => 'drawer', //ドロワーメニューをここに表示すると指定
+            'container' => 'nav',
+            'container_class' => 'drawer-nav',
+            'menu_class' => 'drawer-list',
+            )
+            );
+            ?>
+        </div><!-- /drawer-content -->
+      </div><!-- /drawer -->
 
-				<!-- drawer-content -->
-				<div class="drawer-content">
-					<nav class="drawer-nav">
-						<ul class="drawer-list">
-							<li class="m_icon1 menu-item"><a href="#">メニュー1</a></li>
-							<li class="m_icon2 menu-item"><a href="#">メニュー2</a></li>
-							<li class="m_icon3 menu-item"><a href="#">メニュー3</a></li>
-							<li class="m_icon4 menu-item"><a href="#">メニュー4</a></li>
-							<li class="m_icon5 menu-item"><a href="#">メニュー5</a></li>
-						</ul>
-					</nav>
-				</div><!-- /drawer-content -->
-			</div><!-- /drawer -->
+    </div><!-- /inner -->
+  </header><!-- /header -->
 
-		</div><!-- /inner -->
-	</header><!-- /header -->
-
-	<!-- header-nav -->
-	<nav class="header-nav">
-		<div class="inner">
-			<ul class="header-list">
-				<li class="m_icon1 menu-item"><a href="#">メニュー1</a></li>
-				<li class="m_icon2 menu-item"><a href="#">メニュー2</a></li>
-				<li class="m_icon3 menu-item"><a href="#">メニュー3</a></li>
-				<li class="m_icon4 menu-item"><a href="#">メニュー4</a></li>
-				<li class="m_icon5 menu-item"><a href="#">メニュー5</a></li>
-			</ul>
-		</div><!-- /inner -->
-	</nav><!-- header-nav -->
+  <!-- header-nav -->
+  <nav class="header-nav">
+    <div class="inner">
+      <?php
+        wp_nav_menu(
+        //.header-listを置き換えて、PC用メニューを動的に表示する
+        array(
+        'container' => false,
+        'depth' => 1,
+        'theme_location' => 'global', //グローバルメニューをここに表示すると指定
+        'container' => 'false',
+        'menu_class' => 'header-list',
+        )
+        );
+      ?>
+    </div><!-- /inner -->
+  </nav><!-- header-nav -->
